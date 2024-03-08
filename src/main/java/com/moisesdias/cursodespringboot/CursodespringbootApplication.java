a1f135faaf61be1ed4e14d57ad2dda9e383578cb
MoisesDN <m.dias009@gmail.com>
2024-03-08 15:32:29 -0300

    first commit

---
+++ src/main/java/com/moisesdias/cursodespringboot/CursodespringbootApplication.java
@@ -0,0 +1,14 @@
+package com.moisesdias.cursodespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursodespringbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CursodespringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moisesdias/cursodespringboot/CursodespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moisesdias.cursodespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursodespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
