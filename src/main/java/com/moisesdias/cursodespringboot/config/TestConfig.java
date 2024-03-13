ee6e27afd90c990c65e8d4a702c8c5bdbf982e01
MoisesDN <m.dias009@gmail.com>
2024-03-13 10:17:44 -0300

    OrderStatus enum

--- src/main/java/com/moisesdias/cursodespringboot/config/TestConfig.java
+++ src/main/java/com/moisesdias/cursodespringboot/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.moisesdias.cursodespringboot.config;
 
 import com.moisesdias.cursodespringboot.entities.Order;
 import com.moisesdias.cursodespringboot.entities.User;
+import com.moisesdias.cursodespringboot.entities.enums.OrderStatus;
 import com.moisesdias.cursodespringboot.repositories.OrderRepository;
 import com.moisesdias.cursodespringboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 
 
