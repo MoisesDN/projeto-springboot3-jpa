c16191605f022ec307d6c328869d3a0fc3ec9438
MoisesDN <m.dias009@gmail.com>
2024-03-20 11:06:23 -0300

    Exception handling - findById

--- src/main/java/com/moisesdias/cursodespringboot/entities/Order.java
+++ src/main/java/com/moisesdias/cursodespringboot/entities/Order.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.moisesdias.cursodespringboot.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
@@ -12,6 +13,7 @@ import java.util.Set;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
